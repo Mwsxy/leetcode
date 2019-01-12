e40ac84f0940af6eca4e5c465b7cecf12408e5fa
eytter <xs_yb@foxmail.com>
2019-01-12 18:33:10 +0800

    AC, but slow

---
+++ P6/p6.cpp
@@ -0,0 +1,29 @@
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string res;
+        int k;
+        for (auto i=0;i<numRows;i++) {
+            for (auto t=0;t<s.size();t++) {
+                k=t%(2*(numRows-1));
+                if (k==i) res+=s[t];
+                if (k>=numRows && k%(numRows-1)+i==numRows-1) 
+                    res+=s[t];
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string s="LEETCODEISHIRING";
+    cout << Solution().convert(s,4);
+    return 0;
+}
