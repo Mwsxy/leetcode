333ef700cd3156b8e679410c1268f57522919038
eytter <xs_yb@foxmail.com>
2019-01-12 18:58:26 +0800

    add mode file for copy

---
+++ mode.cpp
@@ -0,0 +1,26 @@
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int xxx() {}
+};
+
+static const auto io_sync_off = []()
+{
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+
+int main() {
+    auto res = Solution().xxx;
+    return 0;
+}
