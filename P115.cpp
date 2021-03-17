f962421f7be44cb2fdc6aa552b56e7760d2fed96
eytter <xs_yb@foxmail.com>
2021-03-17 23:22:14 +0800

    AC P115

--- P115.cpp
+++ P115.cpp
@@ -5,22 +5,34 @@ public:
     const int len = 1001;
     int numDistinct(string s, string t) {
         int n1,n2;
-        n1 = s.size();
-        n2 = t.size();
+        n1 = s.length();
+        n2 = t.length();
         if (n1<n2) return 0;
-        int f[len][len];
-        memset(f, 0, sizeof(f));
-        for (int i = 0; i < n2; i++) {
-            for (int j = i; j < n1; j++) {
-                f[i][j] = max(f[i][j], f[i-1][j]);
-                if (s[j] == t[i]) {
-                    f[i][j] = max(f[i][j], f[i-1][j-1]);
+        vector<vector<long>> f(n1+1, vector<long>(n2+1));
+
+        for (int i=0; i<=n1; i++)
+            f[i][n2] = 1;
+
+        for (int i = n1-1; i >=0; i--) {
+            for (int j = n2-1; j >=0; j--) {
+                if (s[i] == t[j]) {
+                    f[i][j] = f[i+1][j+1]+f[i+1][j];
                 }
+                else 
+                    f[i][j] = f[i+1][j];
             }
         }
+        return f[0][0];
     }
 };
 
 int main() {
+    string s,t;
+    s = "rabbbit";
+    t = "rabbit";
+    cout << Solution().numDistinct(s, t) << endl;
+    s = "babgbag";
+    t = "bag";
+    cout << Solution().numDistinct(s, t) << endl;
     return 0;
 }
